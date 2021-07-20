14590fdf37c944f193afd974e062de68dbf66a96
Alim <shogenovam@apkpro.ru>
2021-07-20 22:51:01 +0300

    fixed filter in Approval table

--- src/Pages/Admin/AdminPayments/components/Approval/Approval.tsx
+++ src/Pages/Admin/AdminPayments/components/Approval/Approval.tsx
@@ -273,6 +273,9 @@ export const Approval: FC<Props> = ({
   }, [currentPage, hubConnection, pageLength, sorting]);
 
   const submitApproval = () => {
+    console.log(openDateApproval.from);
+    console.log(openDateApproval.to);
+
     if (hubConnection) {
       setCurrentPage(1);
       hubConnection
@@ -283,6 +286,7 @@ export const Approval: FC<Props> = ({
           searchSafeIDApproval.length ? searchSafeIDApproval : null,
           openDateApproval.from ? openDateApproval.from : null,
           openDateApproval.to ? openDateApproval.to : null,
+          null,
           null,
           null,
           null,
