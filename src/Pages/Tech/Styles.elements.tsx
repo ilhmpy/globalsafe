de13990eba633e383cde45c570635b06df7ec815
Vladislav <jsih1236@gmail.com>
2021-07-30 10:35:56 +0300

    Layouted tech works page

--- src/Pages/Tech/Styles.elements.tsx
+++ src/Pages/Tech/Styles.elements.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components/macro";
 import { ReactComponent as P404 } from "../../assets/svg/404.svg";
+import { ReactComponent as tech } from "../../assets/svg/tech.svg";
 
 export const TechContainer = styled.div`
     width: 100%;
@@ -16,13 +17,22 @@ export const TechHeader = styled.div`
     margin: 0 auto;
 `;
 
-export const TechDescription = styled.div`
+export const TechDescription = styled.div<{ bold?: boolean; }>`
     text-align: center;
     Font family: Roboto
     color: ${({ theme }) => theme.tech.color};
     font-weight: 400;
     width: 60%;
     margin: 0 auto;
+
+    ${({ bold }) => {
+        if (bold) {
+            return `
+               font-weight: 700;
+               font-size: 2.20em;
+            `;
+        }
+    }}
 `;
 
 export const TechLink = styled.a`
@@ -31,5 +41,9 @@ export const TechLink = styled.a`
 `;
 
 export const Page404 = styled(P404)`
+    margin-bottom: 60px;
+`;
+
+export const Tech = styled(tech)`
     margin-bottom: 60px;
 `;
