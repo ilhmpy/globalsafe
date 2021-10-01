796d58ea20a6b01e7d57eb12e739d5fd9425579f
Alim Shogenov <shogenov@tutu.tech>
2021-10-01 18:09:05 +0300

    end of 01.10.21

--- src/Pages/PrivateArea/Settings/NewPayMethod.tsx
+++ src/Pages/PrivateArea/Settings/NewPayMethod.tsx
@@ -9,11 +9,67 @@ export const NewPayMethod: FC = () => {
 
   return (
     <Container>
-      <Back text="К списку платежных методов" onGoBackClick={() => history.push(routers.settings)} />
+      <Back
+        text="К списку платежных методов"
+        onGoBackClick={() => history.push(routers.settings)}
+      />
 
-      <div>
-        <div>NewPayMethod</div>
-      </div>
+      {/* <TitleWrapper>
+        <Title>Платежный метод АО «Тинькофф Банк»</Title>
+      </TitleWrapper>
+
+      <Blocks>
+        <LeftSide>
+          <Entry>
+            <span>Аккаунт:</span>
+            <span>viproller777</span>
+          </Entry>
+          <Entry>
+            <span>Рейтинг аккаунта:</span>
+            <span>5.0</span>
+          </Entry>
+        </LeftSide>
+
+        <RightSide>
+          <Entry>
+            <span>Платежный метод:</span>
+            <span>{method}</span>
+          </Entry>
+          <Entry>
+            <span>Валюта:</span>
+            <span>{currency}</span>
+          </Entry>
+          <Entry>
+            <span>Номер карты:</span>
+            <span>{cardNumber}</span>
+          </Entry>
+          <Entry>
+            <span>Держатель карты:</span>
+            <span>{cardHolder}</span>
+          </Entry>
+          <Entry>
+            <span>Активность метода:</span>
+
+            <SwitcherRow checked={isActive}>
+              <Switcher
+                onChange={() => {
+                  console.log(111);
+                  setChosenMethod((prev: any) => ({ ...prev, isActive: !prev.isActive }));
+                }}
+                checked={isActive}
+              />
+              <span>{t(isActive ? 'depositsPrograms.on' : 'depositsPrograms.off')}</span>
+            </SwitcherRow>
+          </Entry>
+
+          <ButtonWrapper>
+            <Button bigSize outlinePrimary onClick={() => undefined}>
+              Удалить
+            </Button>
+          </ButtonWrapper>
+        </RightSide>
+      </Blocks>
+     */}
     </Container>
   );
 };
