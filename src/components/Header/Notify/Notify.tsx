f0b4c2f3ee147946b99ab8ce2894b29ff4f6bb62
Vladislav <jsih1236@gmail.com>
2021-10-09 13:18:12 +0300

    .

--- src/components/Header/Notify/Notify.tsx
+++ src/components/Header/Notify/Notify.tsx
@@ -26,28 +26,7 @@ interface NotifyItem {
 };
 
 export const Notify: FC<NotifyProps> = ({ block, auth, admin, setCheckeds }: NotifyProps) => {
-    const [notifies, setNotifies] = useState<any[]>([
-        /*
-        { date: new Date(), view: true, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: -1000, balanceKind: 59, desc: "Средства заморожены в рамках обмена по вашему ордеру на продажу № 47996468. Для завершения обмена подтвердите получение средств в течении 20 минут" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 14, desc: "Адрес кошелька: 377JKD792HcVkP5qZoF7Pv31MbUwke5iMX" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: true, value: 10000, balanceKind: 59, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: 2800000, balanceKind: 14, desc: "Выплата по депозиту Expres" },
-        { date: new Date(), view: true, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: -1000, balanceKind: 59, desc: "Средства заморожены в рамках обмена по вашему ордеру на продажу № 47996468. Для завершения обмена подтвердите получение средств в течении 20 минут" },
-        { date: new Date(), view: true, value: 10000, balanceKind: 14, desc: "Адрес кошелька: 377JKD792HcVkP5qZoF7Pv31MbUwke5iMX" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 59, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: true, value: 2800000, balanceKind: 14, desc: "Выплата по депозиту Expres" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: -1000, balanceKind: 59, desc: "Средства заморожены в рамках обмена по вашему ордеру на продажу № 47996468. Для завершения обмена подтвердите получение средств в течении 20 минут" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 14, desc: "Адрес кошелька: 377JKD792HcVkP5qZoF7Pv31MbUwke5iMX" },
-        { date: new Date(), view: true, value: 10000, balanceKind: 1, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: false, value: 10000, balanceKind: 59, desc: "Баланс личного кабинета успешно пополнен через cwd.global" },
-        { date: new Date(), view: true, value: 2800000, balanceKind: 14, desc: "Выплата по депозиту Expres" },
-        */
-    ]);
+    const [notifies, setNotifies] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const appContext = useContext(AppContext);
     const hubConnection = appContext.hubConnection;   
@@ -85,7 +64,7 @@ export const Notify: FC<NotifyProps> = ({ block, auth, admin, setCheckeds }: Not
     }, [hubConnection]);
     function onNotify(id: string) {
         if (hubConnection) {
-            hubConnection.invoke("SetStateInAppNotification", id, 0)
+            hubConnection.invoke("SetStateInAppNotification", id, 1)
              .then(res => {
                  console.log(res);
              })
