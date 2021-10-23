2f04c1e0d0b5d0300501e80c993a63a92c45038e
Vladislav <jsih1236@gmail.com>
2021-10-23 12:46:05 +0300

    Fixed

--- src/Pages/PrivateArea/components/ui/Counter.tsx
+++ src/Pages/PrivateArea/components/ui/Counter.tsx
@@ -57,7 +57,7 @@ export const Counter: FC<Props> = ({ data, formatNum, over, delay, setTimerDown 
       setTimerDown(true);
     };
     return count > -1 ? state : '0м. 0с.'
-  }
+  };
 
   return <>{getTime()}</>;
 };
