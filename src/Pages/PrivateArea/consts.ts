048b0fc84de1322f8a6282cf56dbcdf63ca51ca1
nickdima <nickdima13384@gmail.com>
2021-11-17 11:28:27 +0300

    modal and valid

--- src/Pages/PrivateArea/consts.ts
+++ src/Pages/PrivateArea/consts.ts
@@ -1,5 +1,5 @@
 export const Device = {
     mobile: `(max-width: 768px)`,
-    tablet: `only screen and (min-device-width: 768px) and (max-device-width: 1024px)`,
+    tablet: `only screen and (min-width: 768px) and (max-width: 1024px)`,
     desktop: `(min-width: 1024px)`,
 };
